164b67e1af5a1bf9d1079e889310816f98cb8af2
Xenhat Liamano <xenhat@polarityviewer.org>
2016-04-20 02:18:37 -0400

    initial commit. Handles warnings and errors.

---
+++ linter.py
@@ -0,0 +1,36 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by xenhat
+# Copyright (c) 2016 xenhat
+#
+# License: MIT
+#
+
+"""This module exports the Lslint plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Lslint(Linter):
+    """Provides an interface to lslint."""
+
+    syntax = 'lsl'
+    # cmd = 'lslint -p'
+    cmd = 'lslint'
+    executable = None
+    version_args = '-V'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 0.4.2'
+    regex = r'((?P<warning> WARN)|(?P<error>ERROR))\:\:\s\(\s+(?P<line>\d+),\s+(?P<col>\d+)\)\:\s(?P<message>.*)'
+    multiline = False
+    line_col_base = (1, 1)
+    tempfile_suffix = 'lsl'
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = None
