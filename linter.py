72ac490b53a1addc523d3562439276ae34064654
Xenhat Liamano <xenhat@polarityviewer.org>
2016-04-21 03:51:16 -0400

    Applying hacks to pass code review :unamused:

--- linter.py
+++ linter.py
@@ -22,8 +22,12 @@ class Lslint(Linter):
     version_args = '-V'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
     version_requirement = '>= 0.4.2'
-    regex = r'((?P<warning> WARN)|(?P<error>ERROR))\:\:\s\(\s+(?P<line>\d+),\s+(?P<col>\d+)\)\:\s(?P<message>.*)'
-    multiline = False
+    regex = r'''(?xi)
+    ((?P<warning> WARN)|(?P<error>ERROR))
+    \:\:\s\(\s+(?P<line>\d+),\s+(?P<col>\d+)
+    \)\:\s(?P<message>.*)
+    '''
+    multiline = True
     line_col_base = (1, 1)
     tempfile_suffix = 'lsl'
     error_stream = util.STREAM_BOTH
