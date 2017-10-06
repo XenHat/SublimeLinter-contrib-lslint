853e4cb2ac11e8d9bd586905b6f3df2a0e17a073
Xenhat <XenHat@users.noreply.github.com>
2017-10-06 13:18:30 -0400

    Fixed: -i switch requires version 1.0.4 or more

--- linter.py
+++ linter.py
@@ -126,7 +126,7 @@ class Lslint(Linter):
     executable = 'lslint'
     version_args = '-V'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.4.2'
+    version_requirement = '>= 1.0.4'
     regex = r'''(?xi)
         (?:(?P<warning> WARN)|(?P<error>ERROR))\:\:\s
         \(\s*(?P<line>\d+),\s*(?P<col>\d+)\)\:\s
